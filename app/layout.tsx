49542cc02112b5f82b7ded1827e9362113ae46a8
Sarmad-47 <sarmadahmad83@gmail.com>
2025-06-13 12:03:54 +0500

    Header & Footer Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,31 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_NAME,APP_DESCRIPTION,SERVER_URL } from "@/lib/constants";
 
 const inter =Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Prostore",
-  description: "A modern ecommerce platform built with Next.js",
+  title: {
+    /**
+     * Useful in multi page applications
+     * You want consistent title across all pages
+     * 
+     */
+    template: `%s | Prostore`, 
+    default: APP_NAME,
+
+    /**
+     * absolute title breaks free from the template 
+     * and sets the title as is.
+     * 
+     * Useful when you want to set a specific title for a page
+     * without the template.
+     */
+   // absolute: "My Title"
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
