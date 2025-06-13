379fbc7103035b2ede625f992484a9153cc2f381
Sarmad-47 <sarmadahmad83@gmail.com>
2025-06-13 15:33:40 +0500

    Theme Mode Toggle and Loading and Not found Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
-import { APP_NAME,APP_DESCRIPTION,SERVER_URL } from "@/lib/constants";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
-const inter =Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
     /**
      * Useful in multi page applications
      * You want consistent title across all pages
-     * 
+     *
      */
-    template: `%s | Prostore`, 
+    template: `%s | Prostore`,
     default: APP_NAME,
 
     /**
-     * absolute title breaks free from the template 
+     * absolute title breaks free from the template
      * and sets the title as is.
-     * 
+     *
      * Useful when you want to set a specific title for a page
      * without the template.
      */
-   // absolute: "My Title"
+    // absolute: "My Title"
   },
   description: APP_DESCRIPTION,
-  metadataBase: new URL(SERVER_URL)
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
@@ -34,11 +35,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
+    /**
+     * We use `suppressHydrationWarning` to prevent hydration errors
+     * Next Themes uses window object to deal with changing of theme
+     * server does not have a window object
+     * so without suppressHydrationWarning it will tell us that there is mismatch between server and client code.
+     * we must stop it from complaining about it.
+     */
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const NotFoundPage = () => {
+  return <div className="flex flex-col items-center justify-center "></div>;
+};
+
+export default NotFoundPage;
