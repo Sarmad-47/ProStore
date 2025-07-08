c71d8abb222526a1d80508929015cc1555a5d9e7
Sarmad Ahmad <sarmadahmad83@gmail.com>
2025-07-08 17:01:56 +0500

    Sign Up Form and Sign In Form

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -14,6 +14,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
   return (
@@ -26,34 +27,28 @@ const Menu = () => {
           </Link>
         </Button>
 
-        <Button asChild>
-          <Link href="/sign-in">
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
+        <UserButton />
       </nav>
 
-      <Sheet>
-        <SheetTrigger className="align-middle">
-          <EllipsisVertical />
-        </SheetTrigger>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
 
-        <SheetContent className="flex flex-col items-start">
-          <SheetTitle>Menu</SheetTitle>
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-          <SheetDescription></SheetDescription>
-        </SheetContent>
-      </Sheet>
+          <SheetContent className="flex flex-col items-start">
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+            <Button asChild variant="ghost">
+              <Link href="/cart">
+                <ShoppingCart /> Cart
+              </Link>
+            </Button>
+            <UserButton />
+            <SheetDescription></SheetDescription>
+          </SheetContent>
+        </Sheet>
+      </nav>
     </div>
   );
 };
